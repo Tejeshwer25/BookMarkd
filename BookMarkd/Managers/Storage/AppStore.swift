3c4b470b12e414e8ba4cdddbaac31a122c8f2cb0
Tejeshwer Singh <tejeshwersingh2504@gmail.com>
2026-02-11 23:42:28 +0530

    Completed the flow for swift data integration in app

--- BookMarkd/Managers/Storage/AppStore.swift
+++ BookMarkd/Managers/Storage/AppStore.swift
@@ -20,21 +20,6 @@ class StorageManageer: ObservableObject {
         self.context = context
     }
     
-    /// Method to get book list from cache if present and if cache is empty it fetched books from persistent storage
-    /// - Returns: Book list
-    func getBookList() -> [BookModel] {
-        if self.bookList.isEmpty {
-            do {
-                self.bookList = try self.getBookListFromSwiftData()
-                return bookList
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
-        
-        return self.bookList
-    }
-    
     
     /// Method to save book to swift data persistent storage and update cache accordingly
     /// - Parameter book: book model to save
@@ -60,7 +45,20 @@ class StorageManageer: ObservableObject {
     /// - Parameter id: book id
     /// - Returns: book
     func getBookWith(id: String) -> BookModel? {
-        return self.bookList.first { $0.id == id }
+        let descriptor = FetchDescriptor<BookModel>(
+            predicate: #Predicate<BookModel> { book in
+                book.id == id
+            },
+            sortBy: []
+        )
+        
+        do {
+            let book = try context.fetch(descriptor).first
+            return book
+        } catch {
+            print(error.localizedDescription)
+            return nil
+        }
     }
     
     /// Method to updated books reading state
@@ -68,21 +66,14 @@ class StorageManageer: ObservableObject {
     ///   - to: updated reading state
     ///   - bookID: book id
     func updateBookReadState(to: BookReadingState, for bookID: String) {
-        self.bookList = self.bookList.map { book in
-            if book.id == bookID {
-                do {
-                    try self.updateBookModelInSwiftData(book, readState: to)
-                    var updatedBook = book
-                    updatedBook.readState = to
-                    return updatedBook
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-            return book
-        }
+        let descriptor = FetchDescriptor<BookModel>(
+            predicate: #Predicate<BookModel> { $0.id == bookID }
+        )
         
-        print(self.bookList)
+        if let book = try? context.fetch(descriptor).first {
+            book.readState = to
+            try? context.save()
+        }
     }
     
     /// Method to add quote to book
@@ -90,19 +81,19 @@ class StorageManageer: ObservableObject {
     ///   - id: id of book for which quote to be added
     ///   - quote: quote to be added
     func addQuoteToBook(id: String, quote: QuotesModel) {
-        self.bookList = self.bookList.map { book in
-            if book.id == id {
-                do {
-                    try self.addQuoteToBook(book,
-                                            quote: quote)
-                    var updatedBook = book
-                    updatedBook.quotes.append(quote)
-                    return updatedBook
-                } catch {
-                    print(error.localizedDescription)
-                }
+        let descriptor = FetchDescriptor<BookModel>(
+            predicate: #Predicate<BookModel> { $0.id == id }
+        )
+        
+        do {
+            if let storedBook = try context.fetch(descriptor).first {
+                quote.book = storedBook
+                storedBook.quotes.append(quote)
+                context.insert(quote)
+                try context.save()
             }
-            return book
+        } catch {
+            print(error.localizedDescription)
         }
     }
     
@@ -111,25 +102,16 @@ class StorageManageer: ObservableObject {
     ///   - rating: new ratings
     ///   - bookId: book id
     func updateBookRating(to rating: Int, for bookId: String) {
-        self.bookList = self.bookList.map { book in
-            if book.id == bookId {
-                do {
-                    try self.updateBookModelInSwiftData(book, rating: rating)
-                    var updatedBook = book
-                    updatedBook.rating = rating
-                    return updatedBook
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
-            return book
+        let descriptor = FetchDescriptor<BookModel>(
+            predicate: #Predicate<BookModel> { $0.id == bookId }
+        )
+        
+        if let book = try? context.fetch(descriptor).first {
+            book.rating = rating
+            try? context.save()
         }
     }
-}
 
-
-// MARK: Swift Data CRUD methods
-extension StorageManageer {
     /// Method to remove book from swift data wishlist storage
     /// - Parameter book: Book to be deleted
     private func removeBookFromSwiftDataWishlist(bookID: String) throws {
@@ -148,55 +130,5 @@ extension StorageManageer {
     private func addBookToSwiftData(_ book: BookModel) throws {
         self.context.insert(book)
         try context.save()
-    }
-    
-    /// Method to get book list from swift data
-    /// - Returns: book list present in persistent storage
-    private func getBookListFromSwiftData() throws -> [BookModel] {
-        let descriptor = FetchDescriptor<BookModel>()
-        let booksFetched = try context.fetch(descriptor)
-        return booksFetched
-    }
-    
-    /// Method to update book data in swift persistent storage
-    /// - Parameters:
-    ///   - book: book to be updated
-    ///   - readState: reading state of book
-    ///   - rating: ratings for the book
-    private func updateBookModelInSwiftData(_ book: BookModel,
-                                            readState: BookReadingState? = nil,
-                                            rating: Int? = nil) throws {
-        let id = book.id
-        let descriptor = FetchDescriptor<BookModel>(
-            predicate: #Predicate<BookModel> { $0.id == id }
-        )
-        
-        if let book = try context.fetch(descriptor).first {
-            if let readState {
-                book.readState = readState
-            }
-            if let rating {
-                book.rating = rating
-            }
-            try context.save()
-        }
-    }
-    
-    /// Method to add quote to a book in swift data storage
-    /// - Parameters:
-    ///   - book: book to which quote will be added
-    ///   - quote: quote to be added
-    private func addQuoteToBook(_ book: BookModel, quote: QuotesModel) throws {
-        let id = book.id
-        let descriptor = FetchDescriptor<BookModel>(
-            predicate: #Predicate { $0.id == id }
-        )
-        
-        if let storedBook = try context.fetch(descriptor).first {
-            quote.book = storedBook
-            storedBook.quotes.append(quote)
-            context.insert(quote)
-            try context.save()
-        }
     }
 }
