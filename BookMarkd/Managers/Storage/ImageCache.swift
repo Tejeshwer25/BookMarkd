fea8656b681750fe8051da7b84131d520a5c6985
Tejeshwer Singh <tejeshwersingh2504@gmail.com>
2026-02-13 23:56:44 +0530

    Implemented image caching

---
+++ BookMarkd/Managers/Storage/ImageCache.swift
@@ -0,0 +1,45 @@
+//
+//  ImageCache.swift
+//  BookMarkd
+//
+//  Created by Tejeshwer Singh on 13/02/26.
+//
+
+import UIKit
+
+actor CachedImageLoaderActor {
+    private var cache = NSCache<NSString, UIImage>()
+    
+    static let shared = CachedImageLoaderActor()
+    private init() {}
+    
+    func load(from url: URL?) async -> UIImage? {
+        guard let url else {
+            return nil
+        }
+        
+        if let cached = await CachedImageLoaderActor.shared.image(for: url) {
+            return cached
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let uiImage = UIImage(data: data) {
+                await CachedImageLoaderActor.shared.set(uiImage, for: url)
+                return uiImage
+            }
+        } catch {
+            print("Image load failed:", error)
+        }
+        
+        return nil
+    }
+    
+    private func image(for url: URL) -> UIImage? {
+        cache.object(forKey: url.absoluteString as NSString)
+    }
+    
+    private func set(_ image: UIImage, for url: URL) {
+        cache.setObject(image, forKey: url.absoluteString as NSString)
+    }
+}
--- BookMarkd/UIComponents/BookImageSkeleton.swift
+++ BookMarkd/UIComponents/BookImageSkeleton.swift
@@ -11,28 +11,34 @@ struct BookImageSkeleton: View {
     var body: some View {
         RoundedRectangle(cornerRadius: 12)
             .foregroundStyle(.gray)
+            .overlay {
+                ProgressView()
+            }
     }
 }
 
 struct BookImage: View {
-    var bookImageURL: String
+    let bookImageURL: String
     var imageFrame: (width: CGFloat, height: CGFloat) = (100, 150)
+    @State private var image: UIImage?
     
     var body: some View {
-        if bookImageURL.isEmpty == false {
-            AsyncImage(url: URL(string: bookImageURL)) { image in
-                image
+        Group {
+            if let image = image {
+                Image(uiImage: image)
                     .resizable()
                     .frame(width: self.imageFrame.width, height: self.imageFrame.height)
                     .clipShape(RoundedRectangle(cornerRadius: 12))
-            } placeholder: {
+            } else {
                 BookImageSkeleton()
                     .frame(width: self.imageFrame.width, height: self.imageFrame.height)
             }
-        } else {
-            BookImageSkeleton()
-                .frame(width: self.imageFrame.width, height: self.imageFrame.height)
-                .foregroundStyle(.red)
+        }
+        .onChange(of: bookImageURL, initial: true) { oldValue, newValue in
+            Task {
+                image = await CachedImageLoaderActor.shared.load(from: URL(string: bookImageURL))
+            }
         }
     }
 }
+
