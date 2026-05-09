f0489735bee12f0d536384227168675ffd0809ac
Tejeshwer Singh <tejeshwersingh2504@gmail.com>
2026-05-09 18:27:16 +0530

    Updated widgets to be dynamic in UI based on their size

--- BookMarkdWidgets/BookMarkdWidgets.swift
+++ BookMarkdWidgets/BookMarkdWidgets.swift
@@ -39,34 +39,106 @@ struct SimpleEntry: TimelineEntry {
 }
 
 struct BookMarkdWidgetsEntryView : View {
+    @Environment(\.widgetFamily) var family
     var entry: Provider.Entry
-    @Query var quotes: [QuotesModel]
-
+    @Query var books: [BookModel]
+    
+    @ViewBuilder
     var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "quote.opening")
-                    .resizable()
-                    .frame(width: 27, height: 20)
-                    .opacity(0.25)
+        let book = books.filter({ $0.quotes.count > 0 }).randomElement()!
+        let quote = book.quotes.randomElement()?.text
+        
+        switch family {
+        case .systemSmall, .systemMedium:
+            VStack(alignment: .center) {
+                HStack {
+                    Image(systemName: "quote.opening")
+                        .resizable()
+                        .frame(width: 27, height: 20)
+                        .opacity(0.25)
+                        .foregroundStyle(.accent)
+                    Spacer()
+                }
+                
                 Spacer()
+                Text(quote ?? "No quote available")
+                    .fontDesign(.serif)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "quote.closing")
+                        .resizable()
+                        .frame(width: 27, height: 20)
+                        .opacity(0.25)
+                        .foregroundStyle(.accent)
+                }
             }
-            
-            Text(quotes.randomElement()?.text ?? "No quote available")
-                .fontDesign(.serif)
-            Spacer()
-            
-            HStack {
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .init(horizontal: .leading, vertical: .top))
+        case .systemLarge:
+            VStack(alignment: .center) {
+                HStack {
+                    Image(systemName: "quote.opening")
+                        .resizable()
+                        .frame(width: 27, height: 20)
+                        .opacity(0.25)
+                        .foregroundStyle(.accent)
+                    Spacer()
+                }
+                
                 Spacer()
-                Image(systemName: "quote.closing")
-                    .resizable()
-                    .frame(width: 27, height: 20)
-                    .opacity(0.25)                
+                Text(quote ?? "No quote available. Add a quote to see it here.")
+                    .fontDesign(.serif)
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                
+                if quote != nil {
+                    Divider()
+                        .foregroundStyle(.accent)
+                    
+                    Text("\(book.title) by \(book.authorName.joined(separator: ", "))")
+                        .font(.caption)
+                        .fontDesign(.serif)
+                        .foregroundStyle(.accent)
+                }
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "quote.closing")
+                        .resizable()
+                        .frame(width: 27, height: 20)
+                        .opacity(0.25)
+                        .foregroundStyle(.accent)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .init(horizontal: .leading, vertical: .top))
+        case .systemExtraLarge:
+            VStack {
+                Text("Hello, World! systemExtraLarge")
+            }
+        case .accessoryCircular:
+            VStack {
+                Text("Hello, World! accessoryCircular")
+            }
+        case .accessoryRectangular:
+            VStack {
+                Text("Hello, World! accessoryRectangular")
+            }
+        case .accessoryInline:
+            VStack {
+                Text("Hello, World! accessoryInline")
+            }
+        @unknown default:
+            VStack {
+                Text("Hello, World! unkn")
             }
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .init(horizontal: .leading, vertical: .top))
+        
     }
 }
+    
 
 struct BookMarkdWidgets: Widget {
     let kind: String = "BookMarkdWidgets"
